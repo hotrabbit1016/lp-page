2e7f9201ba2cdafd985505715208b30c07603097
hotrabbit1016 <hotrabbit1016@gmail.com>
2025-08-17 00:38:05 +0800

    save

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <Container>
         <h1 className="display-4">Smart Liquidity Mining, Simplified.</h1>
         <p className="lead">
-          智能平台，輕鬆管理並最大化您的流動性挖礦收益。
+          Effortlessly manage and maximize your liquidity mining yields with our intelligent, automated platform.
         </p>
         <Button size="lg" className="btn-primary-custom">Get Started</Button>
       </Container>
